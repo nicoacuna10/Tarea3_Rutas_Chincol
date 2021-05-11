a32f67e44da56c10e02eefb1e5c5f070633623aa
CarlosR759 <mipop@pop-os.localdomain>
2021-05-11 19:02:39 -0400

    Menu finalizado y gitignore agregado

--- main.c
+++ main.c
@@ -2,21 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-void menu();
+void menu(int *opcion);
 
-void importarArchivoDeCoordenadas();
-void distanciaEntreEntregas();
-void mostrar5EntregasMasCercanas();
-void crearRuta();
-void generarRutaAleatoria();
-void mejorarRuta();
-void mostrarRutas();
-void mejorRuta();
+//void importarArchivoDeCoordenadas();
+//void distanciaEntreEntregas();
+//void mostrar5EntregasMasCercanas();
+//void crearRuta();
+//void generarRutaAleatoria();
+//void mejorarRuta();
+//void mostrarRutas();
+//void mejorRuta();
 
 
 int main(){
+	int opcion;
 
+	do{
+		menu(&opcion);
+		if(opcion == 0) break;
+		//if(opcion == 1) importarArchivoDeCoordenadas();
+		//if(opcion == 2) distanciaEntreEntregas();
+		//if(opcion == 3) mostrar5EntregasMasCercanas();
+		//if(opcion == 4) crearRuta();
+		//if(opcion == 5) generarRutaAleatoria();
+		//if(opcion == 6) mejorarRuta();
+		//if(opcion == 7) mostrarRutas();
+		//if(opcion == 8) mejorRuta();
+	}while(opcion != 0);
 
-
+	printf(" ----------------------------\n");
+	printf("| Adios !                    |\n");
+	printf("| Que tenga buen dia.        |\n");
+	printf(" ----------------------------\n\n");
 	return 0;
 }
--- menu.c
+++ menu.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 
-void menu(){
+void menu(int *opcion){
+    char entrada[5000];
 
+    do{
+        memset(entrada, '\0', 5000);
+        printf(" ------------------------------------------\n");
+        printf("|       Menu principal del programa        |\n");
+        printf(" ------------------------------------------\n");
+        printf("| Opcion 1: Importar archivo de coordeandas|\n");
+        printf("| Opcion 2: Distancia entre entregas       |\n");
+        printf("| Opcion 3: Mostrar 5 entregas mas cercanas|\n");
+        printf("| Opcion 4: Crear ruta                     |\n");
+        printf("| Opcion 5: Generar ruta aleatoria         |\n");
+        printf("| Opcion 6: Mejorar ruta                   |\n");
+        printf("| Opcion 7: Mostrar rutas                  |\n");
+        printf("| Opcion 8: Mejor ruta                     |\n");
+        printf("| Opcion 0: Salir del programa             |\n");
+        printf(" ------------------------------------------\n\n");
+
+        //Lectura de opción//
+        printf(" Elija una opcion:  ");
+        scanf("%[^\n]s", entrada);
+        getchar();
+
+        if(entrada[1] != '\0' || entrada[0] > '8'){
+            printf(" ---------------------\n");
+            printf("| Entrada no valida!! |\n");
+            printf(" ---------------------\n\n");
+            printf(" -----------------------\n");
+            printf("| ingrese opcion valida |\n");
+            printf(" -----------------------\n\n");
+        }
+    }while( (entrada[0] < '0' || entrada[0] > '8') && (entrada[1] != '\0') );
+
+    //Se entrega valor a int por referencia//
+    *opcion = entrada[0] - 48;
+
+    return;
 }
