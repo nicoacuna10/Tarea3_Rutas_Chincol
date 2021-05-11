7b3a76156ddacb12336b8c808dbeaf5c180b4e68
nicoacuna10 <nicolas.acuna.o@mail.pucv.cl>
2021-05-11 09:14:13 -0400

    Primer cambio

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void menu();
+
+void importarArchivoDeCoordenadas();
+void distanciaEntreEntregas();
+void mostrar5EntregasMasCercanas();
+void crearRuta();
+void generarRutaAleatoria();
+void mejorarRuta();
+void mostrarRutas();
+void mejorRuta();
+
+
+int main(){
+
+
+
+	return 0;
+}
