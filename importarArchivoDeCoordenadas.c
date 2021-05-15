fb403ecd8abd4f303e930617019e6170432f4d0d
Nicolás Acuña <80644036+nicoacuna10@users.noreply.github.com>
2021-05-15 11:26:35 -0400

    Comentarios

--- importarArchivoDeCoordenadas.c
+++ importarArchivoDeCoordenadas.c
@@ -48,7 +48,7 @@ int lower_than_int(void * key1, void * key2) {
 }
 
 void importarArchivoDeCoordenadas(char *nombre_archivo, Map **Entregas_id, int *numeroEntregas){
-
+	
     printf("Ingrese nombre archivo .txt: ");
     scanf("%[^\n]s", nombre_archivo);
     getchar();
@@ -62,13 +62,19 @@ void importarArchivoDeCoordenadas(char *nombre_archivo, Map **Entregas_id, int *
     printf("\nIngrese numero de entregas: ");
     scanf("%d", &(*numeroEntregas) );
 
+    /*Creación de mapa que almacena el id de la entrega, las coordenadas x e y, y un arreglo dinámico
+      con las distancias entre las otras entregas. La clave utilizada para acceder a cada entrega es
+      el id*/
     *Entregas_id = createMap(is_equal_int);
-
+	
+    // Declaración de variables. //
     char *leer_linea, auxX[6], auxY[6], linea[11];
     Entrega *E;
-    
-
     int i, j, l, posicion;
+    
+    /*Se lee cada una de las lineas del archivo de texto, se guarda la coordena x e y, y el id respecto
+      de la iteración. Se tiene en consideración los saltos de espacios antes del primer número y entre
+      los números. Al final de cada iteración se guarda en el mapa.*/
     for(i = 0; i < *numeroEntregas; i++){
 
         E = (Entrega*)malloc(sizeof(Entrega));
@@ -114,16 +120,24 @@ void importarArchivoDeCoordenadas(char *nombre_archivo, Map **Entregas_id, int *
         test = (Entrega*) nextMap(*Entregas_id);
     }
     // FIN TESTING //
-
+	
+    // Se realiza una copia del mapa que se utilizará posteriormente para calcular las distancias. //
     Map *CopiaEntregas_id = createMap(is_equal_int);
-    
     E = (Entrega*)firstMap(*Entregas_id);
     while(E){
         insertMap(CopiaEntregas_id, &E->id, E);
         E = (Entrega*)nextMap(*Entregas_id);
     }
     
-
+    /* Se Calcula la distancia entre las entregas, para eso se necesita recorrer el mapa con la
+       información de las entregas y su copia (creada anteriormente). En cada nueva interación
+       del recorrido del primer mapa, se dimensiona el arreglo distancia con tamaño el numero de
+       entregas. En cada iteración del recorrido de la copia, se compara si las entregas tienen
+       distinto id, si se cumple, se calcula la distancia y se almacena en una determinada
+       posición del arreglo. La posición es respecto del id de la entrega a la que se dirige menos
+       1. Por ejemplo, se calcula la distancia entre las entrega id 1 - 4, el valor debe almacenarse
+       en la casilla 3 del arreglo de la entrega 1. En el caso que los id's sean iguales se almacena
+       el valor cero en la casilla correspondiente.*/
     int diferenciaX, diferenciaY;
 
     Entrega *registro1 = (Entrega*) firstMap(*Entregas_id);
@@ -149,10 +163,9 @@ void importarArchivoDeCoordenadas(char *nombre_archivo, Map **Entregas_id, int *
         registro1 = (Entrega*) nextMap(*Entregas_id);
         
     }
-
-    free(CopiaEntregas_id);
     
-
+    // Se libera la copia del mapa- //
+    free(CopiaEntregas_id);
 
     printf("\n\n");
 
